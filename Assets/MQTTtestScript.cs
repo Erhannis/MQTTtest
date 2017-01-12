0b232bbf933f350ff094b5644dcb5879be14581e
Erhannis Kirran <eyeillus@gmail.com>
2017-01-11 23:16:17 -0500

    Test code for M2Mqtt4Unity.

---
+++ Assets/MQTTtestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text;
+
+public class MQTTtestScript : MonoBehaviour {
+
+	private MqttClient4Unity client;
+
+	// Use this for initialization
+	void Start () {
+		client = new MqttClient4Unity("192.168.0.6", 1883, false, null);
+		client.Connect("Unity");
+		client.MqttMsgPublished += new MqttClient.MqttMsgPublishedEventHandler(OnMessage);
+		client.Subscribe("color_value");
+	}
+
+	private void OnMessage(object sender, MqttMsgPublishedEventArgs e) 
+	{
+		Debug.Log("This is called when the event fires. " + e.ToString());
+	}
+
+	void OnDestroy() {
+		client.Disconnect ();
+	}
+
+	private int i;
+
+	// Update is called once per frame
+	void Update () {
+		client.Publish("color_value", Encoding.ASCII.GetBytes("count " + i));
+		i++;
+	}
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Test code for M2Mqtt4Unity, old and unsupported and only one link still working, but the only thing I've found that allows Unity code to connect to an MQTT server.
+
+This is released under the MIT license.  Frankly, I don't know what license M2Mqtt4Unity was released under; its site didn't say, and the source download was gone.  It was supposedly open source, though.
